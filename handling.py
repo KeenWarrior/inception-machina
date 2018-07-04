020fac3dcf2c5bd5c269603a5aa2fbf5a8a4927d
KeenWarrior <anujgargcse@gmail.com>
2018-07-04 10:50:34 +0530

    Error handling basic code

---
+++ handling.py
@@ -0,0 +1,29 @@
+
+try:
+    p = 4567
+    b = 56456
+
+
+    f = open("ab.txt")
+
+    p = b / 0
+
+
+    # f = open("ab.txt")
+
+    for line in f:
+        print(line)
+
+except FileNotFoundError as e:
+    print(e.filename)
+except Exception as e:
+    print(e)
+except ZeroDivisionError as e:
+    print(e)
+
+# except (FileNotFoundError, ZeroDivisionError) :
+#     print("File not found weghrjnwfeokdwOJIH")
+# except :
+#     print("Divided by zero you moron")
+
+# i = 0/0
